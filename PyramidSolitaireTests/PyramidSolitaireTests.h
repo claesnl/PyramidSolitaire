a4a05430a0072cf381c264045763087197355433
Claes Ladefoged <claesnl@gmail.com>
2012-02-18 03:52:44 +0100

    Added Xcode project

---
+++ PyramidSolitaireTests/PyramidSolitaireTests.h
@@ -0,0 +1,13 @@
+//
+//  PyramidSolitaireTests.h
+//  PyramidSolitaireTests
+//
+//  Created by Claes Ladefoged on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PyramidSolitaireTests : SenTestCase
+
+@end
---
+++ PyramidSolitaireTests/PyramidSolitaireTests.m
@@ -0,0 +1,32 @@
+//
+//  PyramidSolitaireTests.m
+//  PyramidSolitaireTests
+//
+//  Created by Claes Ladefoged on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PyramidSolitaireTests.h"
+
+@implementation PyramidSolitaireTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PyramidSolitaireTests");
+}
+
+@end
